c1a12795a2dfbcba57d903d81e690939e0e43871
Kapil meena <114638376+kapilmeenaJimmy@users.noreply.github.com>
2026-09-11 02:21:03 +0530

    Update 19_Remove_Nth_Node_From_End.cpp
    Refactor removeNthFromEnd to improve readability.

--- Linked_List/19_Remove_Nth_Node_From_End.cpp
+++ Linked_List/19_Remove_Nth_Node_From_End.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
   ListNode *removeNthFromEnd(ListNode *head, int n) {
-    // Dummy node handles the case when we remove the head
     ListNode *dummy = new ListNode(0);
     dummy->next = head;
 
     ListNode *fast = dummy;
     ListNode *slow = dummy;
-
-    // Move fast n steps ahead
     for (int i = 0; i < n; i++) {
       fast = fast->next;
     }
-
-    // Move both until fast reaches the last node
     while (fast->next != nullptr) {
       fast = fast->next;
       slow = slow->next;
     }
-
-    // Remove the nth node from the end
     ListNode *temp = slow->next;
     slow->next = slow->next->next;
     delete temp;
