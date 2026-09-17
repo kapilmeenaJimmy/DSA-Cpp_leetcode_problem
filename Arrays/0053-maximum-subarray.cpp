3e33f24ca09134dba6322b18f197d21be763b559
Kapil meena <114638376+kapilmeenaJimmy@users.noreply.github.com>
2026-09-18 02:15:19 +0530

    Add solution for maximum subarray problem
    Implement the maximum subarray problem using Kadane's algorithm.

---
+++ Arrays/0053-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maximumSum = nums[0];
+
+for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maximumSum = max(maximumSum, currentSum);
+        }
+
+return maximumSum;
+    }
+};
